c282f22a4a82bf688396de128dc96e937a982c2f
Radek <rad.musial@gmail.com>
2019-03-26 11:16:51 +0100

    Updates

---
+++ precache-manifest.eacfd73f893cc0e02452017adf8e11b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "15413a89ded24bd12f4f2c258886b0b2",
+    "url": "/weather-page/static/media/bg.15413a89.jpg"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/weather-page/static/media/41.421c13f1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/weather-page/static/media/17.3c85da3e.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/weather-page/static/media/16.0cf9cad1.png"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/weather-page/static/media/15.39ac499c.png"
+  },
+  {
+    "revision": "6b58c3a3c62251900ccd",
+    "url": "/weather-page/static/js/runtime~main.5654ca36.js"
+  },
+  {
+    "revision": "61b67dd9791e20502bda",
+    "url": "/weather-page/static/js/main.26416a68.chunk.js"
+  },
+  {
+    "revision": "3dcc039f307cb5116de0",
+    "url": "/weather-page/static/js/2.6783dffe.chunk.js"
+  },
+  {
+    "revision": "61b67dd9791e20502bda",
+    "url": "/weather-page/static/css/main.e05f1004.chunk.css"
+  },
+  {
+    "revision": "de5fe0fb3e6e29cef010009d2b4f6618",
+    "url": "/weather-page/index.html"
+  }
+];
